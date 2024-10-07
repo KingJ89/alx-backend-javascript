f05e5c923b845c984ad52ba9b19d2849021355ee
KingJ89 <janmutewera@gmail.com>
2024-10-06 22:06:56 -0700

    Task 9 completed successfully

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  
+  return {
+    ...budget,
+    getIncomeInDollars: () => `$${income}`,
+    getIncomeInEuros: () => `${income} euros`,
+  };
+}
