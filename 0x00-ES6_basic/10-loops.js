7d2cb659487b991163794fd142fb50e6a864be37
KingJ89 <janmutewera@gmail.com>
2024-10-06 22:23:00 -0700

    Task 10 completed consicely

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,7 @@
-export default function appendToEachArrayValue(array, appendString)
-{
-	return array.map(value => appendString + value);
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+  return newArray;
 }
-
