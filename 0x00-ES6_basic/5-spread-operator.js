e02dc900a71f730c504e5e17f10e4ec5ae034957
KingJ89 <janmutewera@gmail.com>
2024-10-06 21:41:20 -0700

    Task 5 completed sucessfully

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,6 +1,5 @@
-concatArrays([1, 2], [3, 4], 'hello');  
-// returns [1, 2, 3, 4, 'h', 'e', 'l', 'l', 'o']
-
-concatArrays(['a'], ['b', 'c'], 'zoo'); 
-// returns ['a', 'b', 'c', 'z', 'o', 'o']
+export default function concatArrays(array1, array2, string) {
+  const concatenatedArray = [...array1, ...array2, ...string];
+  return concatenatedArray;
+}
 
