ac7b2daf2881d06ad3e9f19c2aae1b4e3adf0aeb
KingJ89 <janmutewera@gmail.com>
2024-10-21 03:07:51 +0000

    Task 0 promises completed successfully

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,3 @@
-export default function getResponseFromAPI()
-{
-  return new Promise((resolve, reject) => {
-    // Simulating an API response after 2 seconds
-	  setTimeout(() => {
-	    resolve('API response success');
-    }, 2000);
-	  
-	  // Optional: You could also reject the promise if needed
-	 // reject(new Error('API response failure'));
-});
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
