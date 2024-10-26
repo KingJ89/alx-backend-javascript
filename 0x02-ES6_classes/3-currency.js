3c55cd751e71ee2ee6d5d383a4d80d67531a7c0d
KingJ89 <janmutewera@gmail.com>
2024-10-26 05:20:58 +0000

    Task 3 and 2 completed successfrully

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Setter for code with validation
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = newCode;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name with validation
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Method to display full currency format
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+
