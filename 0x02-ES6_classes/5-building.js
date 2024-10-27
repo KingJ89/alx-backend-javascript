ff430610ce3797187be0c7c13f48c7a0de0e34ed
KingJ89 <janmutewera@gmail.com>
2024-10-27 07:33:43 +0000

    Task 5 building refactorin and debbugging complete

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    // Ensure subclass implements evacuationWarningMessage
-    if (
-      new.target !== Building
-      && this.evacuationWarningMessage === undefined
-    ) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error(
         'Class extending Building must override evacuationWarningMessage',
       );
@@ -12,12 +8,10 @@ export default class Building {
     this.sqft = sqft;
   }
 
-  // Getter for sqft with validation
   get sqft() {
     return this._sqft;
   }
 
-  // Setter for sqft with type-checking
   set sqft(value) {
     if (typeof value !== 'number') {
       throw new TypeError('sqft must be a number');
