987b9dc222dc17e7646a0b4d62dc5f7786ebfe0f
KingJ89 <janmutewera@gmail.com>
2024-11-27 04:43:56 +0000

    Task 0 -console debugging

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to be displayed.
+ */
 function displayMessage(message) {
   console.log(message);
 }
 
 module.exports = displayMessage;
+
